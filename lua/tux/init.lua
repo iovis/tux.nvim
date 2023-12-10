f7bfab0c6cf4da70f192353bc55279e825edd58a
David Marchante <davidmarchan@gmail.com>
2023-12-09 21:16:35 -0500

    Create pane if necessary

--- lua/tux/init.lua
+++ lua/tux/init.lua
@@ -1,5 +1,8 @@
 local tux = {}
 
+---@alias TuxPaneOrientation "horizontal"|"vertical"
+---@alias TuxStrategy "pane"|"window"|"float"
+
 -- TODO:
 -- pane, window, popup
 -- remain on exit
@@ -8,11 +11,15 @@ local tux = {}
 
 ---@class TuxOpts
 tux.default_config = {
+  -- ---@type TuxStrategy
+  -- default_strategy = "pane",
+  ---@class TuxPaneOpts
   pane = {
-    ---@type number pane split size
+    ---@type TuxPaneOrientation
+    orientation = "horizontal",
+    ---@type number Size as percentage
     size = 30,
-    ---@type string tmux target
-    target = "{last}",
+    target = ":.{last}",
   },
 }
 
@@ -23,17 +30,43 @@ tux.setup = function(opts)
   tux.config = vim.tbl_deep_extend("force", tux.default_config, opts)
 end
 
----Run command in Tmux
+---Run command in a Tmux pane
 ---@param command string
----@param opts? TuxOpts
-tux.run = function(command, opts)
-  opts = vim.tbl_deep_extend("force", tux.config, opts or {})
+---@param opts? TuxPaneOpts
+tux.pane = function(command, opts)
+  local tmux_command
 
-  -- Exit copy mode if pane exists
-  tux.exit_copy_mode(opts.pane.target)
+  opts = vim.tbl_deep_extend("force", tux.config.pane, opts or {})
 
-  local tmux_command = tux.parse(command, opts)
+  if tux.number_of_panes() == 1 then
+    tux.create_pane(opts)
+    tmux_command = tux.parse(command)
+  else
+    tux.exit_copy_mode(opts.target)
+    tmux_command = tux.parse(command, opts.target)
+  end
+
   vim.fn.system(tmux_command)
+end
+
+---Create pane
+---@param opts TuxPaneOpts
+tux.create_pane = function(opts)
+  assert(
+    opts.orientation == "horizontal" or opts.orientation == "vertical",
+    'pane.orientation should be "horizontal"|"vertical". Given: ',
+    opts.orientation
+  )
+
+  local orientation
+  if opts.orientation == "horizontal" then
+    orientation = "-v"
+  elseif opts.orientation == "vertical" then
+    orientation = "-h"
+  end
+
+  local command = ("tmux split-window %s -p %d"):format(orientation, opts.size)
+  vim.fn.system(command)
 end
 
 ---Exit copy mode from the given pane
@@ -43,15 +76,27 @@ tux.exit_copy_mode = function(pane)
   vim.fn.system(command)
 end
 
+---Number of panes in current window
+---@return number
+tux.number_of_panes = function()
+  local command = "tmux list-panes | wc -l"
+  return tonumber(vim.fn.system(command)) --[[@as number]]
+end
+
 ---Generate Tmux command
 ---@private
 ---@param command string
----@param opts? TuxOpts
----@return string Command to run
-tux.parse = function(command, opts)
-  command = vim.fn.shellescape(command)
+---@param target? string
+---@return string
+tux.parse = function(command, target)
+  local tmux_command = "tmux send"
 
-  return ("tmux send -t {last} %s Enter"):format(command)
+  if target then
+    tmux_command = ("%s -t %s"):format(tmux_command, target)
+  end
+
+  command = vim.fn.shellescape(command)
+  return ("%s %s Enter"):format(tmux_command, command)
 end
 
 return tux
