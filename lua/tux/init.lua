c21531554f8815cf6a45ea946464dd6a7e24013f
David Marchante <davidmarchan@gmail.com>
2023-12-09 22:12:08 -0500

    Window commands

--- lua/tux/init.lua
+++ lua/tux/init.lua
@@ -4,11 +4,8 @@ local tux = {}
 ---@alias TuxStrategy "pane"|"window"|"float"
 
 -- TODO:
--- pane, window, popup
+-- popup
 -- remain on exit
--- focus
--- Tux command
--- tests
 
 ---@class TuxOpts
 tux.default_config = {
@@ -22,6 +19,13 @@ tux.default_config = {
     size = 30,
     target = ":.{last}",
   },
+  ---@class TuxWindowOpts
+  window = {
+    detached = false,
+    name = nil,
+    ---@type boolean If window with name exists, select instead
+    select = false,
+  },
 }
 
 ---Setup Tux
@@ -32,11 +36,15 @@ tux.setup = function(opts)
 
   vim.api.nvim_create_user_command("Tux", function(ctx)
     tux.run(ctx.args)
+  end, { nargs = "+" }) -- complete = "shellcmd"
+
+  vim.api.nvim_create_user_command("Tuxpane", function(ctx)
+    tux.pane(ctx.args)
   end, { nargs = "+" })
 
-  vim.api.nvim_create_user_command("TuxPane", function(ctx)
-    tux.pane(ctx.args)
-  end, { nargs = "+" }) -- complete = "shellcmd"
+  vim.api.nvim_create_user_command("Tuxwindow", function(ctx)
+    tux.window(ctx.args)
+  end, { nargs = "+" })
 end
 
 ---Run command in Tmux using the default strategy
@@ -45,6 +53,43 @@ tux.run = function(command)
   local strategy = tux.config.default_strategy
 
   tux[strategy](command)
+end
+
+---Run command in a Tmux window
+---@param command string
+---@param opts? TuxWindowOpts
+tux.window = function(command, opts)
+  opts = vim.tbl_deep_extend("force", tux.config.window, opts or {})
+
+  local tmux_command = tux.window_command(command, opts)
+
+  vim.cmd(tmux_command)
+end
+
+---Run command in a Tmux window
+---@private
+---@param command string
+---@param opts TuxWindowOpts
+tux.window_command = function(command, opts)
+  local tmux_command = "tmux new-window"
+
+  if opts.detached then
+    tmux_command = ("%s -d"):format(tmux_command)
+  end
+
+  if opts.name then
+    local name = vim.fn.shellescape(opts.name)
+    tmux_command = ("%s -n %s"):format(tmux_command, name)
+  end
+
+  if opts.select then
+    assert(opts.name and not opts.detached, "`select` requires a `name` and it can't be detached")
+
+    tmux_command = ("%s -S"):format(tmux_command)
+  end
+
+  command = ("$SHELL -i -c %s").format(vim.fn.shellescape(command))
+  return ("silent !%s %s"):format(tmux_command, command)
 end
 
 ---Run command in a Tmux pane
@@ -57,13 +102,13 @@ tux.pane = function(command, opts)
 
   if tux.number_of_panes() == 1 then
     tux.create_pane(opts)
-    tmux_command = tux.parse(command)
+    tmux_command = tux.send_keys(command)
   else
     tux.exit_copy_mode(opts.target)
-    tmux_command = tux.parse(command, opts.target)
+    tmux_command = tux.send_keys(command, opts.target)
   end
 
-  vim.fn.system(tmux_command)
+  vim.cmd(tmux_command)
 end
 
 ---Create pane
@@ -105,7 +150,7 @@ end
 ---@param command string
 ---@param target? string
 ---@return string
-tux.parse = function(command, target)
+tux.send_keys = function(command, target)
   local tmux_command = "tmux send"
 
   if target then
@@ -113,7 +158,7 @@ tux.parse = function(command, target)
   end
 
   command = vim.fn.shellescape(command)
-  return ("%s %s Enter"):format(tmux_command, command)
+  return ("silent !%s %s Enter"):format(tmux_command, command)
 end
 
 return tux
--- spec/parser_spec.lua
+++ spec/parser_spec.lua
@@ -1,16 +1,34 @@
 local tux = require("tux")
 local assert = require("luassert")
 
-describe("tux.parse", function()
-  describe("with a target", function()
-    it("generates a tmux command", function()
-      assert.are.same(tux.parse("ls", "{last}"), [[tmux send -t {last} 'ls' Enter]])
-    end)
-  end)
+describe("tux.send_keys", function()
+  it("generates the right command", function()
+    assert.are.same(tux.send_keys("ls", "{last}"), [[silent !tmux send -t {last} 'ls' Enter]])
 
-  describe("without a target", function()
-    it("generates a tmux command", function()
-      assert.are.same(tux.parse("ls"), [[tmux send 'ls' Enter]])
-    end)
+    assert.are.same(tux.send_keys("ls"), [[silent !tmux send 'ls' Enter]])
+  end)
+end)
+
+describe("tux.window_command", function()
+  it("generates the right command", function()
+    assert.are.same(
+      tux.window_command("cargo watch -x run", { detached = false, select = false, name = nil }),
+      [[silent !tmux new-window 'cargo watch -x run']]
+    )
+
+    assert.are.same(
+      tux.window_command("cargo watch -x run", { detached = true, select = false, name = nil }),
+      [[silent !tmux new-window -d 'cargo watch -x run']]
+    )
+
+    assert.are.same(
+      tux.window_command("htop", { detached = false, select = false, name = "htop" }),
+      [[silent !tmux new-window -n 'htop' 'htop']]
+    )
+
+    assert.are.same(
+      tux.window_command("htop", { detached = false, select = true, name = "htop" }),
+      [[silent !tmux new-window -n 'htop' -S 'htop']]
+    )
   end)
 end)
